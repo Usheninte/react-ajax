70d85958cfaa8a45eb0159ef4e78dcbf2151cd56
Usheninte <jrandangana@gmail.com>
2018-07-14 14:11:53 +0100

    confidence++ in writing react code

--- action.js
+++ action.js
@@ -1,1 +1,47 @@
 //Github Pages LINK === https://usheninte.github.io/react-ajax/fetch.html
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: []
+    };
+  }
+
+  //componentDidMount() - this is a key Lifecycle method
+  //most common use case: starting AJAX calls to load data for your component
+  //can call setState
+
+  componentDidMount() {
+    fetch('https://swapi.co/api/people/')
+    /* .then(function(response) {
+      return response.json()
+    }) */
+    .then(response => response.json())
+    .then(responseData => {
+      this.setState({
+        items: responseData.results
+      });
+    })
+    .catch(error => {
+      console.log('Fetching and parsing data error', error);
+    })
+  }
+  
+  render() {
+    let items = this.state.items;
+    console.log(items);
+    return(
+      <div>
+        {items.map(function(star) {
+          return <p key={star.name}><b>{star.name}</b> has an eye colour of: <b>{star.eye_color}</b></p>;
+        })}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("app")
+);
